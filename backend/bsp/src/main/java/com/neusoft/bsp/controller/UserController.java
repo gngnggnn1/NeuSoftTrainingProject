0ef42c75330a70fa52129305b013eeb7b0179b13
DGHSRailgun <1304196335@qq.com>
2020-07-02 16:56:20 +0800

    20200702_16:56

---
+++ backend/bsp/src/main/java/com/neusoft/bsp/controller/UserController.java
@@ -0,0 +1,132 @@
+package com.neusoft.bsp.controller;
+
+import com.github.pagehelper.PageInfo;
+import com.neusoft.bsp.common.base.BaseController;
+import com.neusoft.bsp.common.base.BaseModel;
+import com.neusoft.bsp.common.base.BaseModelJson;
+import com.neusoft.bsp.common.exception.BusinessException;
+import com.neusoft.bsp.common.validationGroup.DeleteGroup;
+import com.neusoft.bsp.common.validationGroup.InsertGroup;
+import com.neusoft.bsp.common.validationGroup.UpdateGroup;
+import com.neusoft.bsp.user.entity.User;
+import com.neusoft.bsp.user.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@CrossOrigin
+@RestController
+@RequestMapping("/user")
+public class UserController extends BaseController {
+
+    @Autowired
+    UserService userService;
+
+    @PostMapping("/addUser")
+    public BaseModel addUser(@Validated({InsertGroup.class}) @RequestBody User user, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            throw BusinessException.INSERT_FAIL.newInstance(this.getErrorResponse(bindingResult),
+                    new Object[]{user.toString()});
+        } else {
+            BaseModel result = new BaseModel();
+            int i = userService.insert(user);
+            if (i == 1) {
+                result.code = 200;
+                return result;
+            } else {
+                throw BusinessException.INSERT_FAIL;
+            }
+        }
+    }
+
+    @PostMapping("/checkUser")
+    public BaseModelJson<User> checkUserForVue(@RequestBody User user) {
+        Map<String, Object> map = new HashMap<>();
+        map.put("username", user.getUsername());
+        map.put("password", user.getPassword());
+        List<User> users = userService.getAllByFilter(map);
+        if (users.size() == 0) {
+            throw BusinessException.USERNAME_NOT_EXISTS;
+        } else {
+            BaseModelJson<User> result = new BaseModelJson();
+            result.code = 200;
+            return result;
+        }
+    }
+
+    @GetMapping("/getInfo")
+    public BaseModelJson<Map> getInfoForVue() {
+        Map<String, Object> map = new HashMap<>();
+        map.put("roles", new String[]{"admin-token"});
+        map.put("introduction", "I am a super administrator");
+        map.put("avatar", "https://wpimg.wallstcn.com/f778738c-e4f8-4870-b634-56703b4acafe.gif");
+        map.put("name", "Super Admin");
+        BaseModelJson<Map> result = new BaseModelJson();
+        result.data = map;
+        result.code = 200;
+        return result;
+    }
+
+    @PostMapping("/logout")
+    public BaseModelJson<String> logout() {
+        BaseModelJson<String> result = new BaseModelJson();
+        result.data = "success";
+        result.code = 200;
+        return result;
+    }
+
+    @PostMapping("/deleteUser")
+    public BaseModel deleteUser(@Validated({DeleteGroup.class}) @RequestBody User user, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            throw BusinessException.USERID_NULL_ERROR.newInstance(this.getErrorResponse(bindingResult),
+                    new Object[]{user.toString()});
+        } else {
+            BaseModel result = new BaseModel();
+            int i = userService.delete(user.getId());
+            if (i == 1) {
+                result.code = 200;
+                return result;
+            } else {
+                throw BusinessException.DELETE_FAIL;
+            }
+        }
+    }
+    @PostMapping("/updateUser")
+    public BaseModel updateUser(@Validated({UpdateGroup.class}) @RequestBody User user, BindingResult bindingResult) {  //bindingResult用于获得validate的反馈信息
+        if (bindingResult.hasErrors()) {
+            throw BusinessException.USERID_NULL_ERROR.newInstance(this.getErrorResponse(bindingResult),
+                    new Object[]{user.toString()});
+        } else {
+            BaseModel result = new BaseModel();
+            int i =userService.update(user);
+            if(i==1){
+                result.code = 200;
+                return result;
+            }else{
+                throw BusinessException.UPDATE_FAIL;
+            }
+        }
+    }
+
+    @PostMapping("/userlist")
+    public BaseModelJson<PageInfo<User>> getUserList(Integer pageNum, Integer pageSize,
+                                                     @RequestBody User user) {
+        Map<String, Object> map = new HashMap<>();
+        BaseModelJson<PageInfo<User>> result = new BaseModelJson();
+        if (pageNum == null) {
+            pageNum = 1;
+        }
+        if (pageSize == null) {
+            pageSize = 10;
+        }
+        result.code = 200;
+        result.data = userService.getAllByFilter(pageNum, pageSize, map);
+
+        return result;
+    }
+}
